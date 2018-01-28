43be2b6ba4250d0f84d770fc62270243bcf6b510
vFlav <flv691@mail.usask.ca>
2018-01-28 15:56:56 -0600

    Create state queue class.

--- A1/src/stateQueue.py
+++ A1/src/stateQueue.py
@@ -1,1 +1,24 @@
-whatever
+class stateQueue():
+    """State queue class that stores the states."""
+
+    q = None
+
+    def __init__(self):
+    """
+    Constructor that initializes the queue.
+    """
+        self.q = Queue()
+
+    def dequeue(self):
+    """
+    Dequeue/remove the next state from the queue.
+    :return: next state.
+    """
+        return self.q.get()
+
+    def enqueue(self, state):
+    """
+    Enqueue/add the next state to the queue.
+    :param state: the state to be added to the queue.
+    """
+        self.q.put(state)
