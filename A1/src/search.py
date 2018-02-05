be86b886da1704caa735d01f26c3dc7006a3424b
Mahmud Azam (mfa256) <mfa256@mail.usask.ca>
2018-02-05 16:07:07 -0600

    Remove ucs algorithm: astar with h0 covers it

--- A1/src/search.py
+++ A1/src/search.py
@@ -14,7 +14,8 @@ class Search():
     def bfs(problem):
         """
             Search algorithm
-            :param initialState: the initial state that is passed to the algorithm.
+            :param initialState: the initial state that is passed to the
+                                    algorithm.
         """
         # monitor performance stats
         exp_nodes = 0 # number of nodes expanded
@@ -51,7 +52,8 @@ class Search():
     def dfs(problem):
         """
             Search algorithm
-            :param initialState: the initial state that is passed to the algorithm.
+            :param initialState: the initial state that is passed to the
+                                    algorithm.
         """
 
         # monitor performance stats
@@ -86,54 +88,11 @@ class Search():
                     memory = s.getNumEl()
         return []
 
-    def ucs(problem):
-        """
-            Search algorithm
-            :param initialState: the initial state that is passed to the algorithm.
-        """
-
-        # keep track of the states that were seen before
-        seen = {}
-
-        # monitor performance stats
-        exp_nodes = 0 # number of nodes expanded
-        start_time = time.time() # Time we started the search.
-        depth = 0 # the depth of our solution.
-        memory = 0 # the max height of the stack for the entire problem
-
-        q = StateHeap(lambda a,b: a.getCost() == b.getCost(), \
-                      lambda a,b: a.getCost() < b.getCost())
-        q.enqueue(problem.getInitState())
-        while q.isEmpty() is False:
-            curr = q.dequeue()
-            # number of expanded nodes increases every time we pop
-            exp_nodes += 1
-            if p.isGoal(curr.getState()):
-                trace, depth = curr.traceBack()
-                elapsed_time = time.time() - start_time
-                print("UCS search results:", \
-                      "\n---Expanded nodes: ", exp_nodes, \
-                      "\n---Depth: ", depth, " nodes" \
-                      "\n---Time: " , round(elapsed_time*1000,2), "ms"\
-                      "\n---Memory: ", memory, " nodes", \
-                      "\n---Total cost: ", costFunction(trace), \
-                      "\n")
-                return trace
-            else:
-                succ = p.successors(curr)
-                for successor in succ:
-                    if successor.getState() not in seen:
-                        seen[successor.getState()] = True
-                        q.enqueue(successor)
-                # adjust memory used if memory use larger than previous record
-                if memory < q.getNumEl():
-                    memory = q.getNumEl()
-        return []
-
     def astar(problem, h):
         """
             Search algorithm
-            :param initialState: the initial state that is passed to the algorithm.
+            :param initialState: the initial state that is passed to the
+                                    algorithm.
         """
         # keep track of the states that were seen before
         seen = {}
