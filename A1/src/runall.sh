dedef7e976eb81e28d229af6ad1eb43cc6c9ff01
Mahmud Azam (mfa256) <mfa256@mail.usask.ca>
2018-02-11 14:51:15 -0600

    Streamline runall.sh to not run bfs on 3422.txt and remove 2422_2.txt

--- A1/src/runall.sh
+++ A1/src/runall.sh
@@ -2,9 +2,13 @@
 
 TEST_DIR="../tests/test_cases/"
 
-for i in `ls $TEST_DIR`; do
+for i in `ls -1 $TEST_DIR | grep -v "3422.txt"`; do
     echo -e "\n\n________________________________________________________\n$i\n"
     python3 search.py "--run-bfs" < "$TEST_DIR$i"
     echo -e "\nFinished $i\n" 1>&2
 done
+
+echo -e "\n\n________________________________________________________\n3422.txt\n"
+python3 search.py < "$TEST_DIR""3422.txt"
+echo -e "\nFinished 3422.txt\n" 1>&2
 
