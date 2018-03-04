ab59aedc036367a5bc6e3a4c91dcd668c2dea91e
tayabsoomro <isoomro.t@gmail.com>
2018-03-03 19:36:10 -0600

    testing successor function

--- A2/src/testBoard.py
+++ A2/src/testBoard.py
@@ -2,11 +2,23 @@ from board import Board
 
 def main():
     b1 = Board()
-    assert(b1.board != None)
-    print(b1)
+    #assert(b1.board != None)
+    #print(b1)
     b1.initialValues()
     print(b1)
-    print(b1.successors("Player 1"))
-    print(b1.successors("Player 2"))
+
+    print("Queen neighbours:")
+    [print(x) for x in (b1.neighbours((0, 2)))]
+    print("Dragon 1 neighbours:")
+    [print(x) for x in (b1.neighbours((1, 1)))]
+    print("Dragon 2 neighbours:")
+    [print(x) for x in (b1.neighbours((1, 2)))]
+    print("Dragon 3 neighbours:")
+    [print(x) for x in (b1.neighbours((1, 3)))]
+    for i in range(0, 5):
+        print("Neighbour of wight", i, ":")
+        [print(x) for x in b1.neighbours((4, i))]
+    #print(b1)
+    #[print(x) for x in b1.successors("Player 1")]
 if __name__ == '__main__':
     main()
