8fee1686cca0dde186fe93e42c89aea9ade78032
vFlav <flv691@mail.usask.ca>
2018-03-09 14:29:15 -0600

    Will implement game controller to allow players play against AI.

---
+++ A2/src/gameController.py
@@ -0,0 +1,12 @@
+
+class GameController():
+    """
+    Class to control The Dragon Queen game.
+    """
+
+    
+
+    def __init__(self):
+        """
+        Initial constructor that initializes a board.
+        """
